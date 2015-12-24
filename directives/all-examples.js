048286b2097759bc96d70b745f6abeec05c2c09f
Allen Kim <allenhwkim@gmail.com>
2015-12-24 00:57:39 -0500

    removed disqus and added facebook comments

--- directives/all-examples.js
+++ directives/all-examples.js
@@ -22,20 +22,6 @@
       '<script src="'+baseUrl+'/testapp/USGSOverlay.js"></script>'
   };
 
-  /**
-   * reset disqus contents by hashbang url
-   */
-  var refreshDisqus = function(identifier) {
-    window.DISQUS &&
-      DISQUS.reset({
-        reload: true,
-        config: function () {  
-          this.page.identifier = identifier;  
-          this.page.url = "" + window.location
-        }
-      });
-  };
-
   var controller = function($http, $timeout, $location, $element, jvPlunker, jvArrayFilter) {
     var vm = this;
 
@@ -44,7 +30,6 @@
       document.querySelector('iframe').
         setAttribute('src',  baseUrl + "/testapp/" + url.replace(/^!/,''));
       vm.viewIframeSource();
-      refreshDisqus(url);
     };
 
     vm.viewIframeSource = function() {
